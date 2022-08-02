07510ef13b1dc70a5cebbd7f49a2847f58987abd
Carpincho99 <ezegiorgis@hotmail.com>
2022-08-02 18:32:23 -0300

    Initial commit

---
+++ lua/user/autocommands.lua
@@ -0,0 +1,39 @@
+vim.cmd [[
+  augroup _general_settings
+    autocmd!
+   autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf set nobuflisted
+  augroup end
+
+  augroup _git
+    autocmd!
+    autocmd FileType gitcommit setlocal wrap
+    autocmd FileType gitcommit setlocal spell
+  augroup end
+
+  augroup _markdown
+    autocmd!
+    autocmd FileType markdown setlocal wrap
+    autocmd FileType markdown setlocal spell
+  augroup end
+
+  augroup _auto_resize
+    autocmd!
+    autocmd VimResized * tabdo wincmd = 
+  augroup end
+
+  augroup _alpha
+    autocmd!
+    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+  augroup end
+
+  augroup _last_edit_pos
+    autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+  augroup end
+
+]]
+-- vim.cmd "augroup _terminal autocmd TermOpen * startinsert autocmd TermOpen * setlocal nornu augroup end"
+vim.api.nvim_create_autocmd("TermOpen", {command = "startinsert"})
+vim.api.nvim_create_autocmd("BufWritePre", {command = "lua vim.lsp.buf.formatting()"})
