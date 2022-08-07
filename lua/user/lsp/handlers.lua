cdc5eea4ba9420174a1b24436cdac9363fcdc2ea
Carpincho99 <ezegiorgis@hotmail.com>
2022-08-07 00:04:27 -0300

    fix gruvbox, enable float ,o

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -13,26 +13,26 @@ M.setup = function()
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
   end
 
-  -- local config = {
-  --   virtual_text = false,
-  --   signs = {
-  --     active = signs,
-  --   },
-  --   update_in_insert = true,
-  --   underline = true,
-  --   severity_sort = true,
-  --   float = {
-  --     focusable = false,
-  --     style = "minimal",
-  --     border = "rounded",
-  --     source = "always",
-  --     header = "",
-  --     prefix = "",
-  --   },
-  -- }
-  --
-  -- vim.diagnostic.config(config)
-  --
+  local config = {
+    --   virtual_text = false,
+    --   signs = {
+    --     active = signs,
+    --   },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
+
+  vim.diagnostic.config(config)
+
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
     width = 60,
@@ -64,7 +64,7 @@ local function lsp_keymaps(bufnr)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>o", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(
     bufnr,
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -127,7 +127,7 @@ _G.packer_plugins = {
   gruvbox = {
     loaded = true,
     path = "/home/eg/.local/share/nvim/site/pack/packer/start/gruvbox",
-    url = "https://github.com/morhetz/gruvbox"
+    url = "https://github.com/gruvbox-community/gruvbox"
   },
   ["hop.nvim"] = {
     config = { "\27LJ\2\nU\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\tkeys\28etovxqpdygfblzhckisuran\nsetup\bhop\frequire\0" },
